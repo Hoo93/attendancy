aeb82029d4af99c63ead2231f8eb07b3c39dabbf
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2023-11-13 00:56:33 +0900

    [ADD] Respository Class 구현
    [ADD] Respository Class 구현

---
+++ src/dbms/respository.interface.ts
@@ -0,0 +1,46 @@
+import { Database } from "./\bdatabase";
+
+export interface RepositoryInterface<T> {
+  create(data: T): Promise<T>;
+  findOneById(id: number): Promise<T | null>;
+  findAll(): Promise<T[]>;
+  delete(id: number): Promise<boolean>;
+  update(id: number, data: Partial<T>): Promise<boolean>;
+}
+
+class Repository<T> implements RepositoryInterface<T> {
+  async executeQuery(query: string) {
+    const conn = await Database.getConnection();
+    try {
+      const [result] = await conn.query(query);
+      return result;
+    } catch (error) {
+      console.error("Query Execution Error", error);
+      throw new Error("Query Execution Error");
+    } finally {
+      conn.release();
+    }
+  }
+
+  async create(data: T): Promise<T> {
+    throw new Error("create method not implemented");
+  }
+
+  async findAll(): Promise<T[]> {
+    throw new Error("findAll method not implemented");
+  }
+
+  async findOneById(id: number): Promise<T | null> {
+    throw new Error("findOneById method not implemented");
+  }
+
+  async update(id: number, data: Partial<T>): Promise<boolean> {
+    throw new Error("update method not implemented");
+  }
+
+  async delete(id: number): Promise<boolean> {
+    throw new Error("delete method not implemented");
+  }
+}
+
+export default Repository;
