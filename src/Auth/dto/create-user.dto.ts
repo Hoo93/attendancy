384f5d19034d96b50fff4e5facdc59daa45ae93f
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2023-11-14 23:55:50 +0900

    [EDIT] CreateUserDto Class로 구현

--- src/Auth/dto/create-user.dto.ts
+++ src/Auth/dto/create-user.dto.ts
@@ -1,4 +1,20 @@
 import { IsNumber, IsString } from "class-validator";
 import User from "../../User/user";
 
-export interface CreateUserDto extends Omit<User, "id"> {}
+export class CreateUserDto {
+  private readonly name: string;
+  private readonly password: string;
+  private readonly age: number;
+  private readonly phoneNumber: string;
+  private readonly email: string;
+
+  constructor(name: string, password: string, age: number, phoneNumber: string, email: string) {
+    this.name = name;
+    this.password = password;
+    this.age = age;
+    this.phoneNumber = phoneNumber;
+    this.email = email;
+  }
+}
+
+export default CreateUserDto;
