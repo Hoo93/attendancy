02669f76a6affa31e2dcf6e7b9698eb5beef804e
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2023-11-16 23:50:48 +0900

    [EDIT] Test Description 수정

--- test/createUserDto.test.ts
+++ test/createUserDto.test.ts
@@ -1,7 +1,7 @@
 import CreateUserDto from "../src/Auth/dto/create-user.dto";
 
-describe("add function", () => {
-  it("CreateUserDto", () => {
+describe("CreateUserDto 테스트", () => {
+  it("생성자를 통해 Dto 생성", () => {
     const createUserDto = new CreateUserDto("test", "pwd", 30, "010-8098-1398", "skks8922@gmail.com");
     expect(createUserDto.name).toBe("test");
   });
