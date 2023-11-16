0399c4648ecb9bcda5fc24d2ed91afdd49a78d21
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2023-11-16 23:56:54 +0900

    [ADD] validationName 실패 테스트 코드 구현

--- test/createUserDto.test.ts
+++ test/createUserDto.test.ts
@@ -5,4 +5,17 @@ describe("CreateUserDto 테스트", () => {
     const createUserDto = new CreateUserDto("test", "pwd", 30, "010-8098-1398", "skks8922@gmail.com");
     expect(createUserDto.name).toBe("test");
   });
+
+  describe("validationName 테스트", () => {
+    it("한글,영어,숫자로 이루어진 name은 true를 반환한다.", () => {
+      const name = "박상후";
+      const password = "pwd";
+      const age = 30;
+      const phoneNumber = "010-8098-1398";
+      const email = "sksk8922@gmail.com";
+      const createUserDto = new CreateUserDto(name, password, age, phoneNumber, email);
+
+      expect(createUserDto.validationName()).toBe(true);
+    });
+  });
 });
