5559535000d6bc4ce6c1e44d5a337a6f178dd2ce
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2023-11-18 01:42:54 +0900

    [EDIT] validatePassword 운영코드 수정 및 리팩토링

--- src/Auth/dto/create-user.dto.ts
+++ src/Auth/dto/create-user.dto.ts
@@ -1,5 +1,9 @@
 
 export class CreateUserDto {
+
+  private MIN_PASSWORD_LENGTH = 6
+  private MAX_PASSWORD_LENGTH = 12
+
   public readonly name: string;
   public readonly password: string;
   public readonly age: number;
@@ -18,13 +22,13 @@ export class CreateUserDto {
     return /^[가-힣a-zA-Z0-9]+$/.test(this.name);
   }
 
-  validationPassword():Boolean {
-    return /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#*]).{6,13}$/.test(this.password);
+  validatePassword():Boolean {
+    return /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#*])$/.test(this.password);
   }
-현
+
   validatePasswordLength() {
     const passwordLength = this.password.length
-    return passwordLength >= 6 && passwordLength <= 12
+    return passwordLength >= this.MIN_PASSWORD_LENGTH && passwordLength <= this.MAX_PASSWORD_LENGTH
   }
 }
 
