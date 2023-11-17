f632e404da8aa7adfa64f907f5075d27e569ee0d
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2023-11-17 19:32:42 +0900

    [ADD] validationName 메소드 파라미터 테스트 추가

--- test/createUserDto.test.ts
+++ test/createUserDto.test.ts
@@ -7,15 +7,28 @@ describe("CreateUserDto 테스트", () => {
   });
 
   describe("validationName 테스트", () => {
-    it("한글,영어,숫자로 이루어진 name은 true를 반환한다.", () => {
-      const name = "박상후";
-      const password = "pwd";
-      const age = 30;
-      const phoneNumber = "010-8098-1398";
-      const email = "sksk8922@gmail.com";
-      const createUserDto = new CreateUserDto(name, password, age, phoneNumber, email);
+    const name = "박상후";
+    const password = "pwd";
+    const age = 30;
+    const phoneNumber = "010-8098-1398";
+    const email = "sksk8922@gmail.com";
 
+    it("한글,영어,숫자로 이루어진 name은 true를 반환한다.", () => {
+      let username = '박상후'
+      const createUserDto = new CreateUserDto(username, password, age, phoneNumber, email);
       expect(createUserDto.validationName()).toBe(true);
+    });
+
+    it.each([
+      ['John123', true],
+      ['한글이름', true],
+      ['special!@#', false],
+      ['   ', false],
+      ['', false],
+    ])('이름이 %s 인 경우 %p 반환', (name, expected) => {
+      const createUserDto = new CreateUserDto(name, password, age, phoneNumber, email);
+      const result = createUserDto.validationName();
+      expect(result).toBe(expected);
     });
   });
 });
