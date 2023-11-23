ff4e692a9048a7da2fbe8e1d612729efaa4c5310
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2023-11-23 23:36:29 +0900

    [EDIT] User Class 로 구현

--- src/User/user.ts
+++ src/User/user.ts
@@ -1,11 +1,29 @@
-import { IsString } from "class-validator";
+export class User {
+  id: string
+  name: string
+  password: string
+  phoneNumber: string
+  age?: number
+  email?: string
+  no?: number
 
-export interface User {
-  id: number;
-  name: string;
-  password: string;
-  age: number;
-  phoneNumber: string;
+  // @ts-ignore
+  constructor(
+    id: string,
+    password: string,
+    name: string,
+    phoneNumber: string,
+    age?: number,
+    email?: string,
+    no?: number
+  ) {
+    this.no = no
+    this.id = id
+    this.password = password
+    this.phoneNumber = phoneNumber
+    this.name = name
+    this.email = email
+  }
 }
 
-export default User;
+// export default User
