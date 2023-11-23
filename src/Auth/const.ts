db66fa59d45aceec1ca18400e7b2a97146afc1c7
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2023-11-24 00:34:49 +0900

    [ADD] 공통상수 정리

--- src/Auth/const.ts
+++ src/Auth/const.ts
@@ -2,8 +2,9 @@ export const MIN_PASSWORD_LENGTH = 6
 export const MAX_PASSWORD_LENGTH = 12
 export const MIN_NAME_LENGTH = 3
 export const MAX_NAME_LENGTH = 8
-export const INVALID_NAME_LENGTH_ERROR_MESSAGE = `사용자 이름은 ${MIN_NAME_LENGTH} 이상 ${MAX_NAME_LENGTH} 이어야 합니다`;
-export const INVALID_NAME_ERROR_MESSAGE = `사용자 이름은 한글,영어,숫자만 사용 가능합니다.`;
+export const INVALID_NAME_LENGTH_ERROR_MESSAGE = `사용자 이름은 ${MIN_NAME_LENGTH} 이상 ${MAX_NAME_LENGTH} 이어야 합니다`
+export const INVALID_NAME_ERROR_MESSAGE = `사용자 이름은 한글,영어,숫자만 사용 가능합니다.`
 export const INVALID_PASSWORD_LENGTH_ERROR_MESSAGE = `비밀번호 길이는 ${MIN_PASSWORD_LENGTH} 이상 ${MAX_PASSWORD_LENGTH} 이어야 합니다`
-export const INVALID_PASSWORD_ERROR_MESSAGE = '비밀번호는 영어,숫자,특수기호를 1개 이상 포함해야 합니다.'
+export const INVALID_PASSWORD_ERROR_MESSAGE =
+  '비밀번호는 영어,숫자,특수기호를 1개 이상 포함해야 합니다.'
 export const INVALID_EMAIL_ERROR_MESSAGE = '이메일 주소가 유효하지 않습니다.'
