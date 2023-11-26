e444c0b0553abbd2df898570f797e0f613f1feb5
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2023-11-26 23:33:35 +0900

    [EDIT] create API 인터페이스 수정

--- src/User/uesr.repository.ts
+++ src/User/uesr.repository.ts
@@ -1,6 +1,11 @@
-import { Service } from "typedi";
-import Repository from "../dbms/default-repository";
-import User from "./user";
+import { Service } from 'typedi'
+import Repository from '../dbms/default-repository'
+import User from './user'
+import createUserDto from '../Auth/dto/create-user.dto'
 
 @Service()
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+  async create(data: createUserDto): Promise<User> {
+    return
+  }
+}
